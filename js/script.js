5b90d75481a152363ed9c34e4dd50c4ef9b16917
AlexTaglia <alex.tagliabue@gmail.com>
2021-05-26 15:30:58 +0200

    aggiunto JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var x = 'funz';
+var y = 'iona';
+var z = x + y;
+
+console.log(z);
+
+alert("PS5 Out of stock");
+
+document.getElementById('news').innerHTML = "PS5 introvabile fino a metà 2022? Parla Foxconn";
